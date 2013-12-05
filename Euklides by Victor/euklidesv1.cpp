39ba7f28afed756ca0360d43b349157335d31db8
vhung92 <victor.hung@outlook.com>
2013-12-05 09:17:19 +0100

    Added code for euclidian algorithm. v1 has less output than v2, which is more informative.

---
+++ Euklides by Victor/euklidesv1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int a,b,p,q;
+	div_t divres;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	p = a;
+	q = b;
+	divres = div(a, b);
+	while(divres.rem != 0)
+	{
+		p = divres.quot;
+		q = divres.rem;
+		divres = div(p,  q);
+	}
+	printf("gcd(%d, %d) = %d", a, b, divres.quot);
+
+	return 0;
+}
