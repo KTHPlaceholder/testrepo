ddbcbb141fd947dc38a19dc35b00dae53b91443c
John Pertoft <john@pertoft.se>
2013-12-08 08:15:53 +0100

    added main function

--- EulerTotient By John/EulerTotient.hs
+++ EulerTotient By John/EulerTotient.hs
@@ -1,5 +1,9 @@
 -- No IO, use ghci to test this
 
+main = do
+	putStrLn "List of euler totient values for the first 30 integers"
+	putStrLn $ show $ [eulerTotient i | i <- [1..30]] 
+
 eulerTotient :: Integer -> Int
 eulerTotient n = length [i | i <- [1..n-1], coprime n i]
 
@@ -10,3 +14,4 @@ euclid :: Integer -> Integer -> Integer
 euclid a b 
 	| b == 0 = a
 	| otherwise = euclid b (mod a b)
+
