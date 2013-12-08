282f3978ce455f2823cd94f0376d53017b41cb55
lambertsson <lambert002@gmail.com>
2013-12-08 20:22:57 +0100

    Good enough

--- caesarCipherAugust/cipherTestCase.cc
+++ caesarCipherAugust/cipherTestCase.cc
@@ -1,5 +1,15 @@
 #include <cipher.h>
+#include "../gtest-1.7.0/gtest.h"
 
-TEST(WASD, QWERT){
-	EXPECT_EQ(0, FACTORIAL(-1));
+TEST(CipherTest, HandlesEncrypt){
+	EXPECT_EQ("a", encrypt("b", -1));
+	EXPECT_EQ("c", encrypt("b", 1));
 }
+
+TEST(CipherTest, HandlesDecrypt){
+	EXPECT_EQ("b", decrypt("a", -1));
+	EXPECT_EQ("b", decrypt("c", 1));
+}
+
+// Man kan såklart göra fler tester - och kanske speciellt prova övergång mellan
+// första och sista bokstaven i ascii - men det är lite överkurs här imo.
